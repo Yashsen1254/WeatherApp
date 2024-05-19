6072f07addc7f5e870cfbb15a712d1ead5a8415c
Yash-Sen <yashsen0402@gmail.com>
2024-05-19 17:24:05 +0530

    App.js Updated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import './App.css';
-
+import "./App.css";
+import Data from "./data";
+import React from "react";
 function App() {
   return (
     <>
+    <React.Fragment>
+      <div className="container">
       <div className="city">
-            <div className="title">
-              <h2>city</h2>
-              <h3>counter</h3>
-            </div>
-            <div className="mb-icon">
-              <p>In</p>
-            </div>
-            <div className="date-time">
-              <div className="dmy">
-                <div id="txt"></div>
-                <div className="current-time">
-                  clock
-                </div>
-                <div className="current-date">date</div>
-              </div>
-              <div className="temperature">
-                <p>
-                  temperature°<span>C</span>
-                </p>
-              </div>
-            </div>
+        <div className="title">
+          <h2>city</h2>
+          <h3>counter</h3>
+        </div>
+        <div className="mb-icon">
+          <p>In</p>
+        </div>
+        <div className="date-time">
+          <div className="dmy">
+            <div id="txt"></div>
+            <div className="current-time">clock</div>
+            <div className="current-date">date</div>
           </div>
+          <div className="temperature">
+            <p>
+              temperature°<span>C</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <Data />
+      </div>
+      </React.Fragment>
     </>
   );
 }
