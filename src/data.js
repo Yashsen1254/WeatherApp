f3da1c2f144a87179cb15a6471b4c077a4bc0b9a
Yash-Sen <yashsen0402@gmail.com>
2024-05-21 20:17:44 +0530

    Icon Updated

--- src/data.js
+++ src/data.js
@@ -1,59 +1,64 @@
-import React from 'react'
+import React from "react";
 
 function Data() {
   const api = [
     {
-      "coord": {
-        "lon": 70.0722,
-        "lat": 22.4645
+      coord: {
+        lon: 70.0722,
+        lat: 22.4645,
       },
-      "weather": [
+      weather: [
         {
-          "id": 800,
-          "main": "Clear",
-          "description": "clear sky",
-          "icon": "01n"
-        }
+          id: 800,
+          main: "Clear",
+          description: "clear sky",
+          icon: "03d",
+        },
       ],
-      "base": "stations",
-      "main": {
-        "temp": 28.48,
-        "feels_like": 34.09,
-        "temp_min": 28.48,
-        "temp_max": 28.48,
-        "pressure": 1005,
-        "humidity": 84,
-        "sea_level": 1005,
-        "grnd_level": 1002
+      base: "stations",
+      main: {
+        temp: 28.48,
+        feels_like: 34.09,
+        temp_min: 28.48,
+        temp_max: 28.48,
+        pressure: 1005,
+        humidity: 84,
+        sea_level: 1005,
+        grnd_level: 1002,
       },
-      "visibility": 10000,
-      "wind": {
-        "speed": 5.15,
-        "deg": 271,
-        "gust": 8.09
+      visibility: 10000,
+      wind: {
+        speed: 5.15,
+        deg: 271,
+        gust: 8.09,
       },
-      "clouds": {
-        "all": 0
+      clouds: {
+        all: 0,
       },
-      "dt": 1716231736,
-      "sys": {
-        "country": "IN",
-        "sunrise": 1716251851,
-        "sunset": 1716299725
+      dt: 1716231736,
+      sys: {
+        country: "IN",
+        sunrise: 1716251851,
+        sunset: 1716299725,
       },
-      "timezone": 19800,
-      "id": 1269317,
-      "name": "Jamnagar",
-      "cod": 200
-    }
-    ]
-    
+      timezone: 19800,
+      id: 1269317,
+      name: "Jamnagar",
+      cod: 200,
+    },
+  ];
+
   return (
-      <div className="forecast">
-      <div className="forecast-icon">
-      </div>
+    <div className="forecast">
+      <div className="forecast-icon"></div>
       <div className="today-weather">
-        <h3>weather</h3>
+        <h3>
+          <img
+            className="tempweather"
+            src={`https://openweathermap.org/img/wn/${api[0].weather[0].icon}.png`}
+            alt="Icon Image"
+          />
+        </h3>
         <div className="search-box">
           <input
             type="text"
@@ -67,44 +72,30 @@ function Data() {
           </div>
         </div>
         <ul>
-            <div>
-              <li className="cityHead">
-                <p>
-                  {api[0].name} , {api[0].sys.country}
-                </p>
-                <img
-                  className="temp"
-                />
-              </li>
-              <li>
-              Temperature {" "}
-                <span className="temp">
-                 {api[0].main.temp}°C
-                </span>
-              </li>
-              <li>
-                Humidity {" "}
-                <span className="temp">
-                {api[0].main.humidity}%
-                </span>
-              </li>
-              <li>
-                Visibility {" "} 
-                <span className="temp">
-                {api[0].visibility} mi
-                </span>
-              </li>
-              <li>
-                Wind Speed {" "}
-                <span className="temp">
-                {api[0].wind.speed} Km/h
-                </span>
-              </li>
-            </div>
+          <div>
+            <li className="cityHead">
+              <p>
+                {api[0].name} , {api[0].sys.country}
+              </p>
+              <img className="temp" />
+            </li>
+            <li>
+              Temperature <span className="temp">{api[0].main.temp}°C</span>
+            </li>
+            <li>
+              Humidity <span className="temp">{api[0].main.humidity}%</span>
+            </li>
+            <li>
+              Visibility <span className="temp">{api[0].visibility} mi</span>
+            </li>
+            <li>
+              Wind Speed <span className="temp">{api[0].wind.speed} Km/h</span>
+            </li>
+          </div>
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Data
+export default Data;
